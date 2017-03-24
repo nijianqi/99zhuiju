a16213455d2a0ca047e0dd6266c9f6d3e73e5790
nijianqi <1198952901@qq.com>
2017-03-24 10:56:20 +0800

    基础包

---
+++ Application/Admin/Controller/ConfController.class.php
@@ -0,0 +1,65 @@
+<?php
+namespace Admin\Controller;
+
+use Think\Controller;
+class ConfController extends CommonController
+{
+    public function index()
+    {
+        $conf = D('conf');
+        $c = $conf->find(1);
+        $this->assign("c", $c);
+        $this->display();
+    }
+    public function doedit()
+    {
+        $conf = D('conf');
+        $data = $conf->create();
+        $data['open'] = I('open');
+		if ($_FILES['logo']['tmp_name'] != '') {
+            $upload = new \Think\Upload();
+            $upload->maxSize = 3145728;
+            $upload->exts = array('jpg', 'gif', 'png', 'jpeg');
+            $upload->rootPath = './Uploads/';
+            $upload->savePath = '/';
+            $info = $upload->uploadOne($_FILES['logo']);
+            if (!$info) {
+                $this->error($upload->getError());
+            } else {
+                $data['logo'] = $info['savepath'] . $info['savename'];
+            }
+        }
+        $result = $conf->save($data);
+        if ($result > 0) {
+            $this->success('修改成功！', U('index'));
+        } else {
+            $this->error('修改失败！');
+        }
+    }
+	public function webconf(){
+        
+       $path = 'Application/Common/Conf/webconf.php';
+       $file = include $path;      
+       $config = array(
+           'WEB_URL' => I('WEB_URL'),
+		   'WEB_TPT' => I('WEB_TPT'),
+		   'GEETEST_ID' => I('GEETEST_ID'),
+		   'GEETEST_KEY' => I('GEETEST_KEY'),
+		   'WEB_YID' => I('WEB_YID'),
+		   'WEB_TAG' => I('WEB_TAG'),
+       );
+       $res = array_merge($file, $config);
+       $str = '<?php return array(';
+        
+       foreach ($res as $key => $value){
+           $str .= '\''.$key.'\''.'=>'.'\''.$value.'\''.',';
+       };
+       $str .= '); ?>';
+       if(file_put_contents($path, $str)){
+           $this->success('修改成功！', U('index'));
+       }else {
+           $this->error('修改失败！');
+       }
+        
+    }
+}
---
+++ Application/Common/Conf/webconf.php
@@ -0,0 +1,1 @@
+<?php return array('WEB_URL'=>'1','WEB_TPT'=>'default','GEETEST_ID'=>'b733a818b5b0c444592c450ce6495830','GEETEST_KEY'=>'86eefa57c021ca5df572ce9103b411f5','WEB_YID'=>'2126365','WEB_TAG'=>'电视剧,电源',); ?>
